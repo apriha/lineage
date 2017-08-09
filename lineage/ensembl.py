44df92e3b7d57ad5dba28eddb3a238b70118db4b
apriha <apriha@users.noreply.github.com>
2017-08-08 20:53:52 -0700

    Add Ensembl REST client

---
+++ lineage/ensembl.py
@@ -0,0 +1,108 @@
+""" Ensembl REST client.
+
+Notes
+-----
+Modified from https://github.com/Ensembl/ensembl-rest/wiki/Example-Python-Client.
+
+References
+----------
+..[1] Yates et. al. (doi:10.1093/bioinformatics/btu613),
+  http://europepmc.org/search/?query=DOI:10.1093/bioinformatics/btu613
+..[2] Cunningham et. al. (doi:10.1093/nar/gku1010),
+  http://europepmc.org/search/?query=DOI:10.1093/nar/gku1010
+
+"""
+
+"""
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016-2017] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+
+"""
+
+"""
+Copyright (C) 2017 Andrew Riha
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+import json
+import sys
+import time
+import urllib.error
+import urllib.parse
+import urllib.request
+
+
+class EnsemblRestClient(object):
+    def __init__(self, server='http://rest.ensembl.org', reqs_per_sec=15):
+        self.server = server
+        self.reqs_per_sec = reqs_per_sec
+        self.req_count = 0
+        self.last_req = 0
+
+    def perform_rest_action(self, endpoint, hdrs=None, params=None):
+        if hdrs is None:
+            hdrs = {}
+
+        if 'Content-Type' not in hdrs:
+            hdrs['Content-Type'] = 'application/json'
+
+        if params:
+            endpoint += '?' + urllib.parse.urlencode(params)
+
+        data = None
+
+        # check if we need to rate limit ourselves
+        if self.req_count >= self.reqs_per_sec:
+            delta = time.time() - self.last_req
+            if delta < 1:
+                time.sleep(1 - delta)
+            self.last_req = time.time()
+            self.req_count = 0
+
+        try:
+            request = urllib.request.Request(self.server + endpoint, headers=hdrs)
+
+            with urllib.request.urlopen(request) as response:
+                content = response.read().decode('utf-8')
+
+            if content:
+                data = json.loads(content)
+            self.req_count += 1
+
+        except urllib.error.HTTPError as e:
+            # check if we are being rate limited by the server
+            if e.code == 429:
+                if 'Retry-After' in e.headers:
+                    retry = e.headers['Retry-After']
+                    time.sleep(float(retry))
+                    self.perform_rest_action(endpoint, hdrs, params)
+            else:
+                sys.stderr.write(
+                    'Request failed for {0}: Status code: {1.code} Reason: {1.reason}\n'.format(
+                        endpoint, e))
+
+        return data
