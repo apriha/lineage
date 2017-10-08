7e409c61c4941ddf13286b0b48c51e0e825bfcdd
apriha <apriha@users.noreply.github.com>
2017-10-07 17:37:25 -0700

    Update for compatibility with TestPyPI

--- lineage/_version.py
+++ lineage/_version.py
@@ -40,7 +40,7 @@ def get_config():
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "pep440"
+    cfg.style = "pep440-pre"
     cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "lineage-"
     cfg.versionfile_source = "lineage/_version.py"
