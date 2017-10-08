d7f5ac43f42b8268eeb7e9829ca6d9e29eb0238c
apriha <apriha@users.noreply.github.com>
2017-10-07 20:29:57 -0700

    Update for compatibility with Versioneer

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))
+
 import lineage
 
 # https://samnicholls.net/2016/06/15/how-to-sphinx-readthedocs/
