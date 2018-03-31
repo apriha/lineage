3d34665aec3a704f7359f222eff7a5476e369f9b
apriha <apriha@users.noreply.github.com>
2018-03-31 10:20:02 -0700

    Add tests for resources

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -16,8 +16,67 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 
-import urllib.request
+import os
 import warnings
 
-class TestResources(object):
-    pass
+
+def test_get_genetic_map_HapMapII_GRCh37(resource):
+    genetic_map_HapMapII_GRCh37 = resource.get_genetic_map_HapMapII_GRCh37()
+    assert len(genetic_map_HapMapII_GRCh37) == 25
+
+
+def test_get_cytoBand_hg19(resource):
+    cytoBand_hg19 = resource.get_cytoBand_hg19()
+    assert len(cytoBand_hg19) == 862
+
+
+def test_get_knownGene_hg19(resource):
+    knownGene_hg19 = resource.get_knownGene_hg19()
+    assert len(knownGene_hg19) == 82960
+
+
+def test_get_kgXref_hg19(resource):
+    kgXref_hg19 = resource.get_kgXref_hg19()
+    assert len(kgXref_hg19) == 82960
+
+
+def test_download_example_datasets(resource):
+    paths = resource.download_example_datasets()
+
+    for path in paths:
+        if path is None or not os.path.exists(path):
+            warnings.warn('Example dataset(s) not currently available')
+            return
+
+    assert True
+
+
+def test__load_genetic_map_None(resource):
+    result = resource._load_genetic_map(None)
+    assert result is None
+
+
+def test__load_cytoBand_None(resource):
+    result = resource._load_cytoBand(None)
+    assert result is None
+
+
+def test__load_knownGene_None(resource):
+    result = resource._load_knownGene(None)
+    assert result is None
+
+
+def test__load_kgXref_None(resource):
+    result = resource._load_kgXref(None)
+    assert result is None
+
+
+def test__download_file_compress(resource):
+    result = resource._download_file('', '', compress=True)
+    assert result is None
+
+
+def test__download_file_invalid_dir(resource):
+    resource._resources_dir = None
+    result = resource._download_file('', '')
+    assert result is None
