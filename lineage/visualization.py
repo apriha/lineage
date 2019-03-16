5911e5ecf393cf7bdff0bc8771b9ae15ed445f32
Andrew Riha <apriha@gmail.com>
2019-03-15 22:34:02 -0700

    Decrease margins on shared DNA plots

--- lineage/visualization.py
+++ lineage/visualization.py
@@ -165,6 +165,7 @@ def plot_chromosomes(one_chrom_match, two_chrom_match, cytobands, path, title, b
     ax.set_title(title, fontsize=14, fontweight="bold")
     plt.xlabel("Build " + str(build) + " Chromosome Position", fontsize=10)
     print("Saving " + os.path.relpath(path))
+    plt.tight_layout()
     plt.savefig(path)
 
 
