0fc6e065c244eb52e10adff478a81e5717319f70
Andrew Riha <apriha@gmail.com>
2019-04-26 23:27:34 -0700

    Update version style

--- lineage/_version.py
+++ lineage/_version.py
@@ -39,7 +39,7 @@ def get_config():
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "pep440-pre"
+    cfg.style = "pep440-post"
     cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "lineage-"
     cfg.versionfile_source = "lineage/_version.py"
